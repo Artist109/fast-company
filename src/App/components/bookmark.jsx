a72b65375fdd948fbdb0493d93974c38ac52c8fb
Sergey Fadeev <artistsilen@gmail.com>
2022-11-25 01:04:06 +0300

    first commit

---
+++ src/App/components/bookmark.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookMark = ({ status, ...rest }) => {
+  return (
+    <button {...rest}>
+      <i className={"bi bi-bookmark-heart" + (status ? "-fill" : "")}></i>
+    </button>
+  );
+};
+
+export default BookMark;
