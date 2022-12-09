e068363636ffac3b68d8f8adf4cae8a1f75c685b
Sergey Fadeev <artistsilen@gmail.com>
2022-12-09 23:47:35 +0300

    added layouts

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,24 @@
 import React from "react";
-import Users from "./components/users";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import NavBar from "./components/navbar";
+
+import Main from "./layouts/main";
+import Login from "./layouts/login";
+import Users from "./layouts/users";
 
 function App() {
-  return <Users />;
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path={"/"} component={Main}></Route>
+        <Route path={"/login"} component={Login}></Route>
+        <Route path={"/users/:userId?"} component={Users}></Route>
+        <Redirect to={Main} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
---
+++ src/App/components/navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link className="nav-link" to={"/"}>
+              Main
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={"/login"}>
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={"/users"}>
+              Users
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
